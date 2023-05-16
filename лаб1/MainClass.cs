00ff836050b5c3555d114633c41e46a1d9a60828
oleksandra <leksievvv@gmail.com>
2023-05-16 18:12:41 +0300

    Add project files.

---
+++ лаб1/MainClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using лаб1;
+using static лаб1.Example;
+
+namespace лаб1
+{
+    internal class mainclass
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+            Requests requests = new Requests(data);
+            Output output = new Output();
+            output.SelectAll_Output();//1
+            output.OneCategory_Output();//2
+            output.ByGrowth_Output();//3 
+            output.LessThan200_Output();//4
+            output.StartsWith_r_Output();//5
+            output.SeveralSelections_Output();//6
+            output.UsingOfSkipWhile_Output();//7
+            output.SelectionFromTwoLists_Output();//8 ??
+            var groupedCategories = requests.GroupCategories(data.CategoriesList);
+            output.GroupCategories_Output(groupedCategories);//9
+            output.InDifferentOrder_Output();//10
+            output.PriceForAllDishesInACategory_Output();//11 
+            output.UseOfConcat_Output();//12
+            output.CategoriesSeparation_Output();//13
+            output.CategoriesUnion_Output();//14
+            output.OneDishContains_Output();//15 ??
+            output.MenuOk_Output();//16 
+            output.UseOfSum_Output();//17
+            output.LessCalories_Output();//18
+            output.Menu1_Output();//19 
+            output.Categories_Output();//20 
+          
+           
+        }
+    }
+}
---
+++ лаб1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace лаб1
+{
+
+public class Example
+    {
+        
+        public class CategoriesOfMenu
+        {
+            public int CategoriesId { get; set; }
+            public string Category { get; set; }
+            public string Name { get; set; }
+
+            public CategoriesOfMenu(int categoriesid, string category, string name)
+            {
+                this.CategoriesId = categoriesid;
+                this.Category = category;
+                this.Name = name;
+            }
+
+            public CategoriesOfMenu() { }
+
+            public override string ToString()
+            {
+                return string.Format($"id:{this.CategoriesId};  category:{this.Category}; dish name:{this.Name} ");
+            }
+
+        }
+
+        public class Ingredient
+        {
+            public int IngredientId { get; set; }
+            public string IngredientName { get; set; }
+            public int AmountOfCalories { get; set; }
+
+            public Ingredient(int ingredientid, string ingredientname, int amountofcalories)
+            {
+                this.IngredientId = ingredientid;
+                this.IngredientName = ingredientname;
+                this.AmountOfCalories = amountofcalories;
+            }
+            public Ingredient() { }
+            public override string ToString()
+            {
+                return string.Format($"id:{this.IngredientId};  name:{this.IngredientName}; amount of calories:{this.AmountOfCalories} ");
+            }
+        }
+
+        public class Dish
+        {
+            public int DishId { get; set; }
+            public string DishName { get; set; }
+            public int IngredientId { get { return Ingredient.IngredientId; } }
+            Ingredient Ingredient = new Ingredient();
+            public int AmountOfIngredient { get; set; }
+
+
+            public List<Dish> Dishes { get; set; }
+
+            public Dish(int dishid, string dishname, int ingredientid, int amountofingredient)
+            {
+                this.DishId = dishid;
+                this.DishName = dishname;
+                Ingredient.IngredientId = ingredientid;
+                this.AmountOfIngredient = amountofingredient;
+
+            }
+            public Dish() { }
+
+            public override string ToString()
+            {
+                return string.Format($"id:{this.DishId};  dish name:{this.DishName} ingredient:{this.Ingredient.IngredientName} amount:{this.AmountOfIngredient} ");
+            }
+        }
+
+        public class Menu
+        {
+            public int MenuId { get; set; }
+            public int CategoryId { get { return categories.CategoriesId; } } 
+            CategoriesOfMenu categories = new CategoriesOfMenu();
+            public DateOnly Date { get; set; }
+            Dish Dish = new Dish();
+            public string Dishname { get { return Dish.DishName; } }
+            public int Cost { get; set; }
+
+            public Menu(int menuid, int categoryid, DateOnly date, string dishname, int cost)
+            {
+                this.MenuId = menuid;
+                categories.CategoriesId = categoryid;
+                this.Date = date;
+                Dish.DishName = dishname;
+                this.Cost = cost;
+            }
+            public Menu() { }
+
+            public override string ToString()
+            {
+                return string.Format($"id:{this.MenuId}; catid:{categories.CategoriesId}; date:{this.Date}; dish name:{this.Dish.DishName} price:{Cost}");
+            }
+
+        }
+    }
+}
+
+   
+
